7d8743c1e4eda65c89b905e5b03befd6d411bf7e
santhana.vk <santhana.vk@zohocorp.com>
2024-02-02 08:06:00 +0530

    changed

--- scripts/TriggerWorkFlowDispatch.sh
+++ scripts/TriggerWorkFlowDispatch.sh
@@ -1,7 +1,16 @@
-curl -L \
+response = $(curl -L \
   -X POST \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer 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" \
+  -H "Authorization: Bearer $1" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/vskrishnan27/pd-actions/actions/workflows/83875854/dispatches \
-  -d '{"ref":"main"}'
+  -d '{"ref":"main"}')
+
+Msg_to_send = "Workflow Dispatch Triggered..."
+
+if echo "$response" | grep -i -q "bac cred"; then
+    Msg_to_send="Failed to trigger the workflow, please check your Github token. - bad"
+elif echo "$response" | grep -i -q "failed"; then
+    Msg_to_send = "Failed to trigger the workflow, please check your Github token. - failed" 
+
+bash $GITHUB_WORKSPACE/scripts/notify/discordNotify.sh.sh "$Msg_to_send"
--- scripts/triggerWorkFlow.sh
+++ scripts/triggerWorkFlow.sh
@@ -1,6 +1,6 @@
 curl -L \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer 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" \
+  -H "Authorization: Bearer $1" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/vskrishnan27/pd-actions/actions/workflows
 

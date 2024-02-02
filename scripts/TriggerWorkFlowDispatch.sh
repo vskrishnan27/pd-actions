a4a56912390472ecb69ed5474c449965b5cdb2c5
santhana.vk <santhana.vk@zohocorp.com>
2024-02-02 08:29:25 +0530

    workflow dispatch message

--- scripts/TriggerWorkFlowDispatch.sh
+++ scripts/TriggerWorkFlowDispatch.sh
@@ -1,4 +1,6 @@
-response = $(curl -L \
+#!/bin/bash
+
+response=$(curl -L \
   -X POST \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $1" \
@@ -6,11 +8,11 @@ response = $(curl -L \
   https://api.github.com/repos/vskrishnan27/pd-actions/actions/workflows/83875854/dispatches \
   -d '{"ref":"main"}')
 
-Msg_to_send = "Workflow Dispatch Triggered..."
+Msg_to_send="Workflow Dispatch Triggered..."
 
-if echo "$response" | grep -i -q "bac cred"; then
+if echo "$response" | grep -i -q "bad cred"; then
     Msg_to_send="Failed to trigger the workflow, please check your Github token. - bad"
 elif echo "$response" | grep -i -q "failed"; then
-    Msg_to_send = "Failed to trigger the workflow, please check your Github token. - failed" 
+    Msg_to_send="Failed to trigger the workflow, please check your Github token. - failed" 
 
-bash $GITHUB_WORKSPACE/scripts/notify/discordNotify.sh.sh "$Msg_to_send"
+bash "$GITHUB_WORKSPACE/scripts/notify/discordNotify.sh" "$Msg_to_send"
